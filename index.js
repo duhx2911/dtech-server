4e88dd9959c7ac1514f4e461b013840f7e554b8a
duhx2911 <nxd2911@gmail.com>
2023-10-28 14:12:06 +0700

    add server

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const path = require("path");
+var cors = require("cors");
+const mysql = require("mysql2");
+const app = express();
+const port = process.env.PORT || 8000;
+app.use(cors());
+
+const con = mysql.createConnection({
+  host: "sql.freedb.tech",
+  port: 3306,
+  user: "freedb_duhx2911",
+  password: "GrrMhY@mx2&vJT?",
+  database: "freedb_d-tech",
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!!!");
+});
+
+app
+  .route("/products")
+  .get(function (req, res) {
+    let sql = "SELECT * FROM products";
+    con.query(sql, (err, response) => {
+      if (err) {
+        res.send({ status: "error", message: err });
+      } else {
+        res.send({ status: "success", data: response });
+      }
+    });
+  })
+  .post(function (req, res) {
+    let sql = "insert into products set ?";
+    const { body } = req;
+    con.query(sql, body, function (err, response) {
+      if (err) {
+        res.send({ status: "error", message: err });
+      } else {
+        res.send({
+          status: "success",
+          data: { ...body, id: response.insertId },
+        });
+      }
+    });
+  });
+
+app.listen(port);
+console.log("Server started at http://localhost:" + port);
