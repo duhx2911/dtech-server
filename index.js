b5cf5f84df5ac9311438c0ca096c2f7b42debaa1
duhx2911 <nxd2911@gmail.com>
2023-10-30 01:37:49 +0700

    add API get, post, put for categories

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const path = require("path");
 var cors = require("cors");
-const mysql = require("mysql2");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 8000;
 app.use(cors());
 
 const con = mysql.createConnection({
   host: process.env.DB_HOST || "127.0.0.1",
+  port: 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "root",
   database: process.env.DB_NAME || "dtech",
@@ -17,6 +19,9 @@ con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!!!");
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app
   .route("/products")
@@ -44,6 +49,48 @@ app
       }
     });
   });
+
+// Categories
+app
+  .route("/categories")
+  .get(function (req, res) {
+    let sql = "SELECT * FROM categories";
+    con.query(sql, (err, response) => {
+      if (err) {
+        res.send({ status: "error", message: err });
+      } else {
+        res.send({ status: "success", data: response });
+      }
+    });
+  })
+  .post(function (req, res) {
+    let sql = "insert into categories set ?";
+    const { body } = req;
+    con.query(sql, body, function (err, response) {
+      if (err) {
+        res.send({ status: "error", message: err });
+      } else {
+        res.send({
+          status: "success",
+          data: { ...body, id: response.insertId },
+        });
+      }
+    });
+  });
+
+app.route("/category/:id").put(function (req, res) {
+  let sql = "UPDATE categories SET ? WHERE id=?";
+  const { body, params } = req;
+  const { id } = params;
+
+  con.query(sql, [body, id], function (err) {
+    if (err) {
+      res.send({ status: "error", message: err });
+    } else {
+      res.send({ status: "success", data: body });
+    }
+  });
+});
 
 app.listen(port);
 console.log("Server started at http://localhost:" + port);
