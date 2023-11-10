74a74b497a9efdbb4a743335ab46eb1a9e6124ca
duhx2911 <nxd2911@gmail.com>
2023-11-10 16:22:19 +0700

    change connect

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
-const mysql = require("mysql2");
+const mysql = require("mysql");
 
 exports.db = () => {
   // connect mysql
   const con = mysql.createConnection({
-    host: process.env.DB_HOST || "sql.freedb.tech",
+    host: process.env.DB_HOST || "localhost",
     port: 3306,
-    user: process.env.DB_USER || "freedb_duhx2911",
-    password: process.env.DB_PASSWORD || "GrrMhY@mx2&vJT?",
-    database: process.env.DB_NAME || "freedb_d-tech",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "Duc@038201003584",
+    database: process.env.DB_NAME || "dtech",
   });
   con.connect(function (err) {
     if (err) throw err;
