cfe804765498247c8c85c96bcfeb9ff07b444876
duhx2911 <nxd2911@gmail.com>
2023-11-29 15:08:03 +0700

    add upload image to server

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const path = require("path");
 var cors = require("cors");
 const mysql = require("mysql2");
 const bodyParser = require("body-parser");
-const app = express();
-const port = process.env.PORT || 8000;
+const multer = require("multer");
+const path = require("path");
 const { db } = require("./db");
+const fs = require("fs");
 const router = require("./auth/auth.routes");
+const port = process.env.PORT || 8000;
+const app = express();
 
 const authMiddleware = require("./auth/auth.middlewares");
 app.use(cors());
@@ -15,6 +17,64 @@ const con = db();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+/*======================================
+  Upload Image
+========================================*/
+
+// Set Storage
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    console.log(file);
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "_" + file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+app.post("/uploadfile", upload.single("myFile"), function (req, res, next) {
+  const file = req.file;
+
+  if (!file) {
+    const error = new Error("Please upload a file");
+    error.httpStatusCode = 400;
+    return next(error);
+  }
+  const response = {
+    message: "Image mis à jour",
+  };
+  res.send(file);
+});
+
+app.get("/getPhoto/:imageId", function (req, res, next) {
+  const { imageId } = req.params;
+  res.sendFile(__dirname + `/images/${imageId}`);
+});
+
+async function deleteAllFilesInDir(dirPath) {
+  try {
+    const files = fs.readdir(dirPath);
+
+    const deleteFilePromises = files.map((file) =>
+      fs.unlink(path.join(dirPath, file))
+    );
+
+    await Promise.all(deleteFilePromises);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+app.delete("/remove-all-images", async function (req, res, next) {
+  const directory = "/images";
+  await deleteAllFilesInDir(__dirname + directory);
+});
+
+/*======================================
+  Auth
+========================================*/
 
 app.use("/auth", router);
 
@@ -162,6 +222,30 @@ app
       }
     });
   });
+
+app.route("/authorization").post(function (req, res) {
+  let sql = "INSERT INTO authorization SET ?";
+  const { body } = req;
+
+  con.query(sql, body, function (err) {
+    if (err) {
+      res.send({ status: "error", message: err });
+    } else {
+      res.send({ status: "success", data: body });
+    }
+  });
+});
+app.route("/authorization/:idAuth").delete(function (req, res) {
+  let sql = "DELETE FROM authorization WHERE id=?";
+  const { idAuth } = req.params;
+  con.query(sql, idAuth, function (err) {
+    if (err) {
+      res.send({ status: "error", message: err });
+    } else {
+      res.send({ status: "success", message: idAuth });
+    }
+  });
+});
 
 app.listen(port);
 console.log("Server started at http://localhost:" + port);
